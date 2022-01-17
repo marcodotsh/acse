8d646c27ad4f96198b14432cffcad9473f7ce7b0
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2022-01-17 04:28:00 +0100

    simrv32im: Log an error message when execution stops because of a simulation fault.

--- simrv32im/simrv32im.c
+++ simrv32im/simrv32im.c
@@ -14,8 +14,15 @@ int main(int argc, char *argv[])
    ldrLoadBinary(argv[1], 0);
 
    status = svInit();
-   while (status == CPU_STATUS_OK) {
+   while (status == SV_STATUS_RUNNING) {
       status = svVMTick();
+   }
+   if (status == SV_STATUS_MEMORY_FAULT) {
+      fprintf(stderr, "Memory fault at address 0x%08x, execution stopped.\n",
+            memGetLastFaultAddress());
+   } else if (status == SV_STATUS_ILL_INST_FAULT) {
+      fprintf(stderr, "Illegal instruction at address 0x%08x\n",
+            cpuGetRegister(CPU_REG_PC));
    }
    return 0;
 }
