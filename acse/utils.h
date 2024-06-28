01ac8e465f4fe634b3042104ec0831b0e7f00aab
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2024-06-28 23:54:22 +0200

    acse: Remove now unused macro.

--- acse/utils.h
+++ acse/utils.h
@@ -20,12 +20,6 @@
 /** Minimum between two values. */
 #define MIN(x, y) ((x) > (y) ? (y) : (x))
 
-/** Arithmetic shift to the right.
- *  The C language does not guarantee the right shift of a signed value is an
- *  arithmetic shift, so we need to use this macro. */
-#define SHIFT_RIGHT_ARITH(x, y) \
-    (((x) >> (y)) | ((x) < 0 ? (((1 << (y)) - 1) << MAX(32 - (y), 0)) : 0))
-
 /** In debug builds (NDEBUG not defined), prints a message on the standard
  * output like `printf'. Otherwise, does nothing and returns zero. */
 int debugPrintf(const char *fmt, ...);
